c2eaba596e7b94c6bcdb1dea476b6164aa7faf5c
ks-unik <ks.a.filippova@yandex.ru>
2026-01-14 18:30:23 +0300

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -12,7 +12,7 @@ class Helper:
         attachment_type=AttachmentType.JSON
     )
         
-        def attach_step_status(self, step_name: str, status_code: int):
-            print(f"[STEP] {step_name} — статус код {status_code}")
-            with allure.step(f"{step_name} — статус код {status_code}"):
-                pass
+    def attach_step_status(self, step_name: str, status_code: int):
+        print(f"[STEP] {step_name} — статус код {status_code}")
+        with allure.step(f"{step_name} — статус код {status_code}"):
+            pass
